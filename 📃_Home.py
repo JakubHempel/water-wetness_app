d1f6c8bcd4e8d06c5474f82b67c975a875f45338
Jakub Hempel <100939247+JakubHempel@users.noreply.github.com>
2025-04-18 14:46:31 +0200

    Add try block with StringIO module

--- 📃_Home.py
+++ 📃_Home.py
@@ -3,6 +3,11 @@ import geemap.foliumap as geemap
 
 st.set_page_config(layout="wide", page_title="📃 Home | HydroPix 💧🛰️")
 
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
 @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name)
